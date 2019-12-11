e34aa0df762bb0609ab6357bfe34e6e6e512e996
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-12-11 16:35:02 +0100

    Add a create-react-app example folder and also small cleanup in the installed packages

---
+++ example/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Product demo</header>
+    </div>
+  );
+};
+
+export default App;
