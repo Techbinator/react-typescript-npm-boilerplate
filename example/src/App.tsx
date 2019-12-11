297aa334fe740efa2f2b7ea8d3d24b68c6914832
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-12-11 16:57:39 +0100

    Import and use the boilerplate in the example folder

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import TestPlugin from 'react-typescript-npm-boilerplate';
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">Product demo</header>
+      <TestPlugin text="aaa" />
     </div>
   );
 };
