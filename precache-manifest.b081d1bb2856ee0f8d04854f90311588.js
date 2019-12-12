f0e9f585872d2496095bf7e872afd80563c26fa0
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-12-12 13:37:42 +0100

    Updates

---
+++ precache-manifest.b081d1bb2856ee0f8d04854f90311588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499ac52f23bb1553e5a045c0c8ab470f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951a0f52710d90eae539",
+    "url": "/static/js/2.774de7da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.774de7da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f71bac183a4503b5b65c",
+    "url": "/static/js/main.19746778.chunk.js"
+  },
+  {
+    "revision": "b021d9a1da770ea02917",
+    "url": "/static/js/runtime-main.4acfb9c0.js"
+  }
+]);
